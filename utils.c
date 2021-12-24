9c22b5bcf99d5dfbfe22e55b8b3b63e1d00ada78
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2021-12-25 00:00:35 +0200

    Add comments that explain each function's purpose

--- utils.c
+++ utils.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include "utils.h"
 
+//Prints a point (vector) a of size d
 void printPoint(double *a, int d){
     printf("( ");
     for (int i = 0; i < d; i++){
@@ -12,6 +13,7 @@ void printPoint(double *a, int d){
     printf(")");
 }
 
+//Fills a point (vector) a of dize d with random elements
 void createPoint(double *a, int d){
     time_t t;
     srand((unsigned) time(&t));
@@ -22,6 +24,7 @@ void createPoint(double *a, int d){
     }
 }
 
+//Takes two points (vectors) of size d and calculates their distance
 double calculateDistance(double *a, double *b, int d){
     double sum = 0;
     for (int i = 0; i < d; i++){
@@ -31,27 +34,28 @@ double calculateDistance(double *a, double *b, int d){
     return distance;
 }
 
+//Swaps two numbers in memory
 void swap(double *a, double *b){
     double temp = *a;
     *a = *b;
     *b = temp;
 }
 
-// Partition using Lomuto partition scheme
+//Partition function used in quickselect
 int partition(double *a, int left, int right, int pIndex){
-    // pick `pIndex` as a pivot from the array
+    //Pick pIndex as a pivot from the array
     double pivot = a[pIndex];
  
-    // Move pivot to end
+    //Move pivot to end
     swap(&a[pIndex], &a[right]);
  
-    // elements less than the pivot will be pushed to the left of `pIndex`;
-    // elements more than the pivot will be pushed to the right of `pIndex`;
-    // equal elements can go either way
+    //Elements less than the pivot will be pushed to the left of pIndex;
+    //Elements more than the pivot will be pushed to the right of pIndex;
+    //Equal elements can go either way
     pIndex = left;
  
-    // each time we find an element less than or equal to the pivot, `pIndex`
-    // is incremented, and that element would be placed before the pivot.
+    //Each time we find an element less than or equal to the pivot, pIndex
+    //is incremented, and that element would be placed before the pivot.
     for (int i = left; i < right; i++){
         if (a[i] <= pivot){
             swap(&a[i], &a[pIndex]);
@@ -59,44 +63,42 @@ int partition(double *a, int left, int right, int pIndex){
         }
     }
  
-    // move pivot to its final place
+    //Move pivot to its final place
     swap(&a[pIndex], &a[right]);
  
-    // return pIndex (index of the pivot element)
+    //Return pIndex (index of the pivot element)
     return pIndex;
 }
  
-// Returns the k'th smallest element in the list within `left…right`
-// (i.e., left <= k <= right). The search space within the array is
-// changing for each round – but the list is still the same size.
-// Thus, `k` does not need to be updated with each round.
+//Returns the k-th smallest element in the list within left…right
 double quickselect(double *nums, int left, int right, int k){
     // If the array contains only one element, return that element
     if (left == right) {
         return nums[left];
     }
  
-    // select `pIndex` between left and right
+    //Select pIndex between left and right
     int pIndex = left + rand() % (right - left + 1);
  
     pIndex = partition(nums, left, right, pIndex);
  
-    // The pivot is in its final sorted position
+    //The pivot is in its final sorted position
     if (k == pIndex) {
         return nums[k];
     }
  
-    // if `k` is less than the pivot index
+    //If k is less than the pivot index
     else if (k < pIndex) {
         return quickselect(nums, left, pIndex - 1, k);
     }
  
-    // if `k` is more than the pivot index
+    //If k is more than the pivot index
     else {
         return quickselect(nums, pIndex + 1, right, k);
     }
 }
 
+//Prints a 2D array of size x X y
 void print_2D(double **X, int x, int y){
     for(int i = 0; i < x; i++){
         for(int j = 0; j < y; j++){
@@ -106,6 +108,7 @@ void print_2D(double **X, int x, int y){
     }
 }
 
+//Prints a 3D array, by printing z 2D arrays
 void print_3D(double ***X, int x, int y, int z){
     for (int k = 0; k < z; k++){
         //Since we can't print 3D arrays, we print all 2D arrays (there are z of them)
@@ -125,12 +128,14 @@ void print_3D(double ***X, int x, int y, int z){
     }
 }
 
+//Returns a 1D column (or a point) from a 3D array. Writes result in the result vector
 void get_1D_col(double ***X, int x, int y, int z, int point, int proc, double* result){
     for (int i = 0; i < x; i++){
         result[i] = X[i][point][proc];
     }
 }
 
+//Returns a 2D array from a 3D array. Writes result in the result array
 void get_2D_arr(double ***X, int x, int y, int z, int proc, double **result){
     for (int i = 0; i < x; i++){
         for (int j = 0; j < y; j++){
@@ -139,6 +144,7 @@ void get_2D_arr(double ***X, int x, int y, int z, int proc, double **result){
     }
 }
 
+//Fills a 3D array with random elements
 void fill_3D(double ***X, int x, int y, int z){
     for(int i = 0; i < x; i++){
         for (int j = 0; j < y; j++){
@@ -150,12 +156,14 @@ void fill_3D(double ***X, int x, int y, int z){
     }
 }
 
+//Sets a 1D column (or point) in a 3D array
 void set_1D_col(double ***X, int x, int y, int z, int point, int pid, double *col){
         for (int i = 0; i < x; i++){
             X[i][point][pid] = col[i];
         }
 }
 
+//Swaps two 1D columns in a 3D array
 void swap_cols(double ***X, int x, int y, int z, int point1, int pid1, int point2,  int pid2){
     double *col1 = (double *)malloc(x * sizeof(double));
     double *col2 = (double *)malloc(x * sizeof(double));
