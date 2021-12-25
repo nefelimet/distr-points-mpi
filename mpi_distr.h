bd49d5557a45dd8905e766ebd3ff82f67604cb49
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2021-12-25 04:19:38 +0200

    Update mpi_distr.h

--- mpi_distr.h
+++ mpi_distr.h
@@ -2,6 +2,6 @@
 #define MPI_DISTR_H
 
 void distributeByMedian(int pid, int numTasks, MPI_Status mpistat, MPI_Request mpireq, int p_left, int p_right, double ***X, int N, int p, int d, double *arr);
-void distributeAll(double ***X, int N, int p, int d);
+void distributeAll(double ***X, int N, int p, int d, int p_left, int p_right);
 
 #endif
